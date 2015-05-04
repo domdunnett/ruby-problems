4facb1b7a93bc86620d036f2873c3575111b6fe9
Dominic Michael Joshua Dunnett <domdunnett@gmail.com>
2015-05-04 17:11:36 +0800

    method to check string for a Palindrome

---
+++ isPalindrome.rb
@@ -0,0 +1,13 @@
+#Palindrome
+
+#Write a function to check if a given string is a palindrome
+
+def isPalindrome(string)
+	if string === string.reverse
+		puts "Yes"
+	else
+		puts "No"
+	end
+end
+
+isPalindrome("hannah")

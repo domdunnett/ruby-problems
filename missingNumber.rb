e59205b7c56272441f752b98281b58c4a448506e
Dominic Michael Joshua Dunnett <domdunnett@gmail.com>
2015-05-05 13:46:12 +0800

    fix missing number bug

--- missingNumber.rb
+++ missingNumber.rb
@@ -2,8 +2,14 @@
 
 #You have an array of numbers 1 to 100 in an array. Only one number is missing in the array. The array is unsorted. Find the missing number.
 
-def missingNumber(arr, num)
+def missingNumber(arr)
 	arr_total = 0
-	arr.each { |i| i += arr_total }
-	arr_total - num
+	arr.each { |i| arr_total += i }
+	puts arr_total
+	missing_number = 5050 - arr_total 
+	missing_number
 end
+
+array = (1..100).to_a
+array.delete(68)
+puts missingNumber(array)
